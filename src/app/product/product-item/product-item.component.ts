f5301ba0f1a90e105e12e06f3d904705a5414001
Philipp Schwarzmaier <pschwarzmaier@gmx.at>
2023-06-28 13:52:48 +0200

    add Dep. angular Material

--- src/app/product/product-item/product-item.component.ts
+++ src/app/product/product-item/product-item.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../product.service';
 import { Observable } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss'],
 })
